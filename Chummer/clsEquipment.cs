bffa38741a64951dc6a2f02f60237fd9d4adfd15
BodieSullivan <bodiesullivan@gmail.com>
2015-04-04 10:58:15 -0500

    #135 - Number of boxes for Drone condition monitor incorrect

--- Chummer/clsEquipment.cs
+++ Chummer/clsEquipment.cs
@@ -14392,6 +14392,17 @@ namespace Chummer
 			}
 		}
 
+        /// <summary>
+        /// Is this vehicle a drone?
+        /// </summary>
+        public Boolean IsDrone
+        {
+            get
+            {
+                return Category.Contains("Drone");
+            }
+        }
+
 		/// <summary>
 		/// Handling.
 		/// </summary>
@@ -14534,15 +14545,32 @@ namespace Chummer
 			}
 		}
 
+        /// <summary>
+        /// Base Physical Boxes. 12 for vehicles, 6 for Drones.
+        /// </summary>
+        public int BasePhysicalBoxes
+        {
+            get
+            {
+                int basePhysicalBoxes = 12;
+
+                if (this.IsDrone)
+                {
+                    basePhysicalBoxes = 6;
+                }
+                return basePhysicalBoxes;
+            }
+        }
+
 		/// <summary>
 		/// Physical Condition Monitor boxes.
 		/// </summary>
 		public int PhysicalCM
 		{
-			get
-			{
-				return 12 + Convert.ToInt32(Math.Ceiling(Convert.ToDouble(_intBody, GlobalOptions.Instance.CultureInfo) / 2.0));
-			}
+            get
+            {
+                return BasePhysicalBoxes + Convert.ToInt32(Math.Ceiling(Convert.ToDouble(_intBody, GlobalOptions.Instance.CultureInfo) / 2.0));
+            }
 		}
 
 		/// <summary>
