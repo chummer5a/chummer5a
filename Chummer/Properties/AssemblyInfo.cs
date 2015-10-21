1a776f1196a9453952d70941f0332163f168e27e
Chummer <chummer5isalive@gmail.com>
2015-10-22 00:01:26 +1100

    Fiddling with shit for appveyor.

--- Chummer/Properties/AssemblyInfo.cs
+++ Chummer/Properties/AssemblyInfo.cs
@@ -32,9 +32,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.5.176.0")]
 [assembly: AssemblyFileVersion("0.5.176.0")]
