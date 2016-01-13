0db4bd31658b67e18fe0a119f44027f9ac2428d6
chummer5a <chummer5isalive@gmail.com>
2016-01-13 12:03:55 +1100

    - It's now always possible to add Internal accessories to weapons.

--- Chummer/Selection Forms/frmSelectWeaponAccessory.cs
+++ Chummer/Selection Forms/frmSelectWeaponAccessory.cs
@@ -55,6 +55,7 @@ namespace Chummer
 				if (strAllowedMount != "")
 					strMount += "contains(mount, \"" + strAllowedMount + "\") or ";
 			}
+			strMount += "contains(mount, \"" + "Internal" + "\") or ";
 			strMount += "mount = \"\"";
 			XmlNodeList objXmlAccessoryList = _objXmlDocument.SelectNodes("/chummer/accessories/accessory[(" + strMount + ") and (" + _objCharacter.Options.BookXPath() + ")]");
 			foreach (XmlNode objXmlAccessory in objXmlAccessoryList)
