bd0e96b6e71a50090b330804f9a4a4fb75f2d537
AngelForest <enkeli.metsa@gmail.com>
2016-02-18 23:01:42 +1000

    Fancy Blocks charsheet changes: Split cyberlimb name and its stats on two lines to make whole section narrower Marks on equipped armor Displaying notes on everything Rewriten Matrix Devices section - attached programs/mods/etc are printed in 2 columns now Change sorting in ID/Credsticks section, so SIN and Licences will always be before credsticks Bold text in general character info and magic section Underlined text for skill categories Displaying some data for lifestyles Some sections won't be displayed if they are empty
    Code changes:
    Source/page fields for martial arts techniques.

--- Chummer/frmCareer.cs
+++ Chummer/frmCareer.cs
@@ -11145,7 +11145,7 @@ namespace Chummer
 				// Open the Martial Arts XML file and locate the selected piece.
 				XmlDocument objXmlDocument = XmlManager.Instance.Load("martialarts.xml");
 
-                XmlNode objXmlAdvantage = objXmlDocument.SelectSingleNode("/chummer/martialarts/martialart[name = \"" + objMartialArt.Name + "\"]/techniques/technique[name = \"" + frmPickMartialArtAdvantage.SelectedAdvantage + "\"]");
+                XmlNode objXmlAdvantage = objXmlDocument.SelectSingleNode("/chummer/techniques/technique[name = \"" + frmPickMartialArtAdvantage.SelectedAdvantage + "\"]");
 
 				// Create the Improvements for the Advantage if there are any.
 				TreeNode objNode = new TreeNode();
