b5481d367965285c8f70d3116f0c685a82c6cf3e
OLStefan <stefan.niewerth@gmx.net>
2016-06-16 15:30:35 +0200

    Fixed a Bug, where Specializations for Knowledge Skills would cost Knowledge Skill Points even without the House rule. Fixes #929.

--- Chummer/Backend/Skills/KnowledgeSkill.cs
+++ Chummer/Backend/Skills/KnowledgeSkill.cs
@@ -285,11 +285,15 @@ namespace Chummer.Skills
 		{
 			if (HasRelatedBoost())
 			{
-				return (Ibase + ((string.IsNullOrWhiteSpace(Specialization) || BuyWithKarma) ? 0 : 1) + 1)/2;
+				return (Ibase + ((string.IsNullOrWhiteSpace(Specialization) || BuyWithKarma || 
+                    CharacterObject.BuildMethod == CharacterBuildMethod.Karma || 
+                    CharacterObject.BuildMethod == CharacterBuildMethod.LifeModule) ? 0 : 1) + 1)/2;
 			}
 			else
 			{
-				return Ibase + ((string.IsNullOrWhiteSpace(Specialization) || BuyWithKarma) ? 0 : 1);
+				return Ibase + ((string.IsNullOrWhiteSpace(Specialization) || BuyWithKarma || 
+                    CharacterObject.BuildMethod == CharacterBuildMethod.Karma || 
+                    CharacterObject.BuildMethod == CharacterBuildMethod.LifeModule) ? 0 : 1);
 			}
 
 		}
