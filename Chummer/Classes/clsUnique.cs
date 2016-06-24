45490299dfd0a28e6f2556d1b75c74b6c854a361
Johannes Elgaard <joha4270@gmail.com>
2016-06-24 10:40:53 +0200

    Removed debug throw that should not be there

--- Chummer/Classes/clsUnique.cs
+++ Chummer/Classes/clsUnique.cs
@@ -751,8 +751,6 @@ namespace Chummer
 			MetatypeMinimum = Convert.ToInt32(strMin);
 			MetatypeMaximum = Convert.ToInt32(strMax);
 			MetatypeAugmentedMaximum = Convert.ToInt32(strAug);
-
-			throw new NotSupportedException();
 		}
 
 		/// <summary>
