10c1563c72d53d424498e65ecfeac7d394eb546f
chummer5a <chummer5isalive@gmail.com>
2016-09-19 10:30:15 +1000

    - Updated Assemblyversion to 5.184.

--- Chummer/Properties/AssemblyInfo.cs
+++ Chummer/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("5.183.0")]
-[assembly: AssemblyFileVersion("5.183.0")]
+[assembly: AssemblyVersion("5.184.0")]
+[assembly: AssemblyFileVersion("5.184.0")]
