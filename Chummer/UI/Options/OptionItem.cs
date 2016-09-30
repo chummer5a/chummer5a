46d9183a3c35e1fa2306817046f32838bcd19d6c
Johannes Elgaard <joha4270@gmail.com>
2016-09-30 08:24:15 +0200

    Save and load _should_ work

--- Chummer/UI/Options/OptionItem.cs
+++ Chummer/UI/Options/OptionItem.cs
@@ -56,23 +56,35 @@ namespace Chummer.UI.Options
 				Wrapper w = new Wrapper(this, tuple.Item1, tuple.Item2, strDisplayName, strTooltip);
                 w.ReadFrom(w.Target());
                 w.SetPos(i);
-
+                _contents.Add(w);
                 i++;
             }
         }
 
         public void WriteBack(object target = null)
         {
-            if (target != null) _target = target;
-            if(_target == null)
+            if (target == null) target = _target;
+            if (target == null)
                 throw new InvalidOperationException("target is never set, cannot work with null data");
+
+            foreach (Wrapper item in _contents)
+            {
+                item.WriteTo(target);
+            }
+
+
         }
 
         public void ReadFrom(object target = null)
         {
-            if (target != null) _target = target;
-            if (_target == null)
+            if (target == null) target = _target;
+            if (target == null)
                 throw new InvalidOperationException("target is never set, cannot work with null data");
+
+            foreach (Wrapper item in _contents)
+            {
+                item.ReadFrom(target);
+            }
         }
 
         private class Wrapper
