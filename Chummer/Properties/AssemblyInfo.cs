8375a30eebad6854b686e8838e31015379f5c9c0
chummer5a <chummer5isalive@gmail.com>
2016-10-10 23:30:26 +1100

    - Build increment.

--- Chummer/Properties/AssemblyInfo.cs
+++ Chummer/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("5.184.0")]
-[assembly: AssemblyFileVersion("5.184.0")]
+[assembly: AssemblyVersion("5.185.0")]
+[assembly: AssemblyFileVersion("5.185.0")]
