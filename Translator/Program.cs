ded6308c3d1dd96c66b296bf1b281a6ce6af6faa
chummer5a <chummer5isalive@gmail.com>
2016-10-23 14:23:25 +1100

    - Added the translator into the repo. - Changed the save method for the translator to consistently be tabs.  Fixes #874. Should also fix #866.

---
+++ Translator/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Windows.Forms;
+
+namespace Translator
+{
+	internal static class Program
+	{
+		[STAThread]
+		private static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new frmMain());
+		}
+	}
+}
---
+++ Translator/Properties/Resources.Designer.cs
@@ -0,0 +1,50 @@
+using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace Translator.Properties
+{
+	[CompilerGenerated]
+	[DebuggerNonUserCode]
+	[GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+	internal class Resources
+	{
+		private static System.Resources.ResourceManager resourceMan;
+
+		private static CultureInfo resourceCulture;
+
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static CultureInfo Culture
+		{
+			get
+			{
+				return Resources.resourceCulture;
+			}
+			set
+			{
+				Resources.resourceCulture = value;
+			}
+		}
+
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static System.Resources.ResourceManager ResourceManager
+		{
+			get
+			{
+				if (Resources.resourceMan == null)
+				{
+					Resources.resourceMan = new System.Resources.ResourceManager("Translator.Properties.Resources", typeof(Resources).Assembly);
+				}
+				return Resources.resourceMan;
+			}
+		}
+
+		internal Resources()
+		{
+		}
+	}
+}
