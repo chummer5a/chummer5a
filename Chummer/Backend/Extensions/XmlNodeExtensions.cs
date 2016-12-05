0cb25e6cada7ab2b8f4b9497038a8f9f23b79932
chummer5a <chummer5isalive@gmail.com>
2016-12-05 13:38:53 +1100

    - Merge

--- Chummer/Backend/Extensions/XmlNodeExtensions.cs
+++ Chummer/Backend/Extensions/XmlNodeExtensions.cs
@@ -37,72 +37,80 @@ namespace Chummer.Backend
                 else
                 {
                     XmlNodeList objXmlTargetNodeList = objXmlParentNode.SelectNodes(objXmlOperationChildNode.Name);
-                    // default is "any", replace with switch() if more check modes are necessary
-                    boolOperationChildNodeResult = false;
-                    if (objXmlOperationChildNode?.Attributes?["checktype"]?.InnerText == "all")
-                        boolOperationChildNodeResult = true;
-
-                    boolOperationChildNodeResult = boolSubNodeResult = boolInvert;
-                    foreach (XmlNode objXmlTargetNode in objXmlTargetNodeList)
+                    // If we're just checking for existance of a node, no need for more processing
+                    if (objXmlOperationChildNode.Attributes?["operation"]?.InnerText == "exists")
                     {
-                        boolSubNodeResult = boolInvert;
-                        if (objXmlTargetNode.SelectNodes("*").Count > 0)
-                        {
-                            if (objXmlOperationChildNode.SelectNodes("*").Count > 0)
-                                boolSubNodeResult = processFilterOperationNode(objXmlTargetNode, objXmlOperationChildNode, objXmlOperationChildNode.Attributes?["OR"] != null) != boolInvert;
-                        }
-                        else
-                        {
-                            strOperationType = "==";
-                            if (objXmlOperationChildNode.Attributes["operation"] != null)
-                                strOperationType = objXmlOperationChildNode.Attributes["operation"].InnerText;
-                            // Note when adding more operation cases: XML does not like the "<" symbol as part of an attribute value
-                            switch (strOperationType)
-                            {
-                                case "doesnotequal":
-                                case "notequals":
-                                case "!=":
-                                    boolInvert = !boolInvert;
-                                    goto case "==";
-                                case "lessthan":
-                                    boolInvert = !boolInvert;
-                                    goto case ">=";
-                                case "lessthanequals":
-                                    boolInvert = !boolInvert;
-                                    goto case ">";
-
-                                case "like":
-                                case "contains":
-                                    boolSubNodeResult = objXmlTargetNode.InnerText.Contains(objXmlOperationChildNode.InnerText) != boolInvert;
-                                    break;
-                                case "greaterthan":
-                                case ">":
-                                    boolSubNodeResult = (System.Convert.ToInt32(objXmlTargetNode.InnerText) > Convert.ToInt32(objXmlOperationChildNode.InnerText)) != boolInvert;
-                                    break;
-                                case "greaterthanequals":
-                                case ">=":
-                                    boolSubNodeResult = (Convert.ToInt32(objXmlTargetNode.InnerText) >= Convert.ToInt32(objXmlOperationChildNode.InnerText)) != boolInvert;
-                                    break;
-                                case "equals":
-                                case "==":
-                                default:
-                                    boolSubNodeResult = (objXmlTargetNode.InnerText == objXmlOperationChildNode.InnerText) != boolInvert;
-                                    break;
-                            }
-                        }
+                        boolOperationChildNodeResult = (objXmlTargetNodeList.Count > 0) != boolInvert;
+                    }
+                    else
+                    {
+                        // default is "any", replace with switch() if more check modes are necessary
+                        boolOperationChildNodeResult = false;
                         if (objXmlOperationChildNode.Attributes?["checktype"]?.InnerText == "all")
+                            boolOperationChildNodeResult = true;
+
+                        boolSubNodeResult = boolInvert;
+                        foreach (XmlNode objXmlTargetNode in objXmlTargetNodeList)
                         {
-                            if (!boolSubNodeResult)
+                            boolSubNodeResult = boolInvert;
+                            if (objXmlTargetNode.SelectNodes("*").Count > 0)
                             {
-                                boolOperationChildNodeResult = false;
+                                if (objXmlOperationChildNode.SelectNodes("*").Count > 0)
+                                    boolSubNodeResult = processFilterOperationNode(objXmlTargetNode, objXmlOperationChildNode, objXmlOperationChildNode.Attributes?["OR"] != null) != boolInvert;
+                            }
+                            else
+                            {
+                                strOperationType = "==";
+                                if (objXmlOperationChildNode.Attributes?["operation"] != null)
+                                    strOperationType = objXmlOperationChildNode.Attributes["operation"].InnerText;
+                                // Note when adding more operation cases: XML does not like the "<" symbol as part of an attribute value
+                                switch (strOperationType)
+                                {
+                                    case "doesnotequal":
+                                    case "notequals":
+                                    case "!=":
+                                        boolInvert = !boolInvert;
+                                        goto case "==";
+                                    case "lessthan":
+                                        boolInvert = !boolInvert;
+                                        goto case ">=";
+                                    case "lessthanequals":
+                                        boolInvert = !boolInvert;
+                                        goto case ">";
+
+                                    case "like":
+                                    case "contains":
+                                        boolSubNodeResult = objXmlTargetNode.InnerText.Contains(objXmlOperationChildNode.InnerText) != boolInvert;
+                                        break;
+                                    case "greaterthan":
+                                    case ">":
+                                        boolSubNodeResult = (System.Convert.ToInt32(objXmlTargetNode.InnerText) > Convert.ToInt32(objXmlOperationChildNode.InnerText)) != boolInvert;
+                                        break;
+                                    case "greaterthanequals":
+                                    case ">=":
+                                        boolSubNodeResult = (Convert.ToInt32(objXmlTargetNode.InnerText) >= Convert.ToInt32(objXmlOperationChildNode.InnerText)) != boolInvert;
+                                        break;
+                                    case "equals":
+                                    case "==":
+                                    default:
+                                        boolSubNodeResult = (objXmlTargetNode.InnerText == objXmlOperationChildNode.InnerText) != boolInvert;
+                                        break;
+                                }
+                            }
+                            if (objXmlOperationChildNode.Attributes?["checktype"]?.InnerText == "all")
+                            {
+                                if (!boolSubNodeResult)
+                                {
+                                    boolOperationChildNodeResult = false;
+                                    break;
+                                }
+                            }
+                            // default is "any", replace above with a switch() should more than two checktypes be required
+                            else if (boolSubNodeResult)
+                            {
+                                boolOperationChildNodeResult = true;
                                 break;
                             }
-                        }
-                        // default is "any", replace above with a switch() should more than two checktypes be required
-                        else if (boolSubNodeResult)
-                        {
-                            boolOperationChildNodeResult = true;
-                            break;
                         }
                     }
                 }
