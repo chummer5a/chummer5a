03e18987d5c33f372d6a5a41bce3eb9d4dec8db5
Johannes Elgaard <joha4270@gmail.com>
2016-12-20 16:42:32 +0100

    Added checkmark icons and slight restucture on images

--- Chummer/Properties/Resources.Designer.cs
+++ Chummer/Properties/Resources.Designer.cs
@@ -263,6 +263,26 @@ namespace Chummer.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap checkbox_check {
+            get {
+                object obj = ResourceManager.GetObject("checkbox_check", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap checkbox_checked {
+            get {
+                object obj = ResourceManager.GetObject("checkbox_checked", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap cog_edit {
             get {
                 object obj = ResourceManager.GetObject("cog_edit", resourceCulture);
