33dfc1633bab774bcda0ebc48101379b4c8933fa
Michael Jesse <michael.jesse@hotmail.com>
2017-02-12 14:53:31 -0600

    Initial check-in

---
+++ Chummer.WPF.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chummer.WPF.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
