7d64601575c7594d33cb4a7d497bb0c5d87c278a
Chummer 5 Is Alive <chummer5isalive@gmail.com>
2017-04-07 12:38:01 +1000

    - Fixed an issue that prevented knowledge skills from selecting their proper default attribute. Fixes #1566.

--- Chummer/Backend/Skills/KnowledgeSkill.cs
+++ Chummer/Backend/Skills/KnowledgeSkill.cs
@@ -136,8 +136,9 @@ namespace Chummer.Skills
 	    {
 	        if (name == null) return;
             //TODO: Should this be targeted against guid for uniqueness? Creating a knowledge skill in career always generates a new SkillId instead of using the one from skills.
-	        XmlNode category = XmlManager.Instance.Load("skills.xml").SelectSingleNode($"chummer/knowledgeskills/skill[name = \"{name}\"]/category");
-	        _type = category?.InnerText ?? "";
+	        XmlNode skillNode = XmlManager.Instance.Load("skills.xml").SelectSingleNode($"chummer/knowledgeskills/skill[name = \"{name}\"]");
+	        _type = skillNode?["category"].InnerText ?? "";
+	        AttributeObject = CharacterObject.GetAttribute(skillNode?["attribute"].InnerText ?? "LOG");
 	    }
 
 		public override string SkillCategory
