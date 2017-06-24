f8be873faa10edf1273ead0c36925713af23b974
Chummer 5 Is Alive <chummer5isalive@gmail.com>
2017-06-24 18:44:30 +1000

    - Fixed load method for old attributes to properly separate base and metatype minimum. Fixes #1739 - Fixed attribute value calculation. Fixes #1744. Fixes #1742

--- Chummer/Backend/Attributes/Attribute.Core.cs
+++ Chummer/Backend/Attributes/Attribute.Core.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
+using System.Windows;
 using System.Xml;
 using Chummer.Datastructures;
 
@@ -82,9 +83,19 @@ namespace Chummer.Backend.Attributes
 				_intBase = 0;
 			}
 			//Converts old attributes to split metatype minimum and base. Saves recalculating Base - TotalMinimum all the time. 
-			if (objNode["value"] != null && BaseUnlocked)
+			if (objNode["value"] != null)
 			{
-				_intBase = Math.Max(_intBase - _intMetatypeMin, 0);
+				int i = Convert.ToInt32(objNode["value"].InnerText);
+				i -= _intMetatypeMin;
+				if (BaseUnlocked)
+				{
+					_intBase = Math.Max(_intBase - _intMetatypeMin, 0);
+					i -= _intBase;
+				}
+				if (i > 0)
+				{
+					_intKarma = i;
+				}
 			}
 			_enumCategory = ConvertToAttributeCategory(objNode["category"]?.InnerText, _strAbbrev);
             _intAugModifier = Convert.ToInt32(objNode["augmodifier"].InnerText);
@@ -244,7 +255,7 @@ namespace Chummer.Backend.Attributes
         {
             get
             {
-	            return Math.Min(Math.Max(Base + Karma, MetatypeMinimum), MetatypeMaximum);
+	            return Math.Min(Base + Karma + MetatypeMinimum, TotalMaximum);
             }
         }
 
