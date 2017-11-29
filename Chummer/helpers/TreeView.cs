c37787e4d73335f8dd13607f11133e7e199bed56
CFresquet <CFresquet@yahoo.com>
2017-11-28 22:08:21 -0800

    added Enchantments to switch statement

--- Chummer/helpers/TreeView.cs
+++ Chummer/helpers/TreeView.cs
@@ -1,4 +1,4 @@
-/*  This file is part of Chummer5a.
+﻿/*  This file is part of Chummer5a.
  *
  *  Chummer5a is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -160,6 +160,10 @@ namespace Chummer.helpers
                 case "Rituals":
                     objSpellTypeNode = Nodes[5];
                     break;
+                case "Enchantments":
+                    objSpellTypeNode = Nodes[6];
+                    break;
+
             }
             objSpellTypeNode.Nodes.Add(objNode);
             objSpellTypeNode.Expand();
