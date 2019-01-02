7d0d1fc043d0d87297854f9be298893eb1c33a4a
ArchonMegalon <archon.megalon@gmail.com>
2019-01-02 09:16:08 +0100

    .net core 2.2

--- ChummerHub/Config.cs
+++ ChummerHub/Config.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 
 namespace ChummerHub
