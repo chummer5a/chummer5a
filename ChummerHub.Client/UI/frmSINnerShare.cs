aca812c622f0144248b62d22942683411edb608b
ArchonMegalon <archon.megalon@gmail.com>
2019-07-25 16:09:27 +0200

    Sharing Dialog implemented. Bugfixes for chummer:// protocol-Handling

---
+++ ChummerHub.Client/UI/frmSINnerShare.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChummerHub.Client.UI
+{
+    public partial class frmSINnerShare : Form
+    {
+        public frmSINnerShare()
+        {
+            InitializeComponent();
+        }
+
+        public ucSINnerShare MyUcSINnerShare
+        {
+            get { return ucSINnerShare1; }
+        }
+
+        
+    }
+}
