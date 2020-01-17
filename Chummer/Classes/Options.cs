80b4e9d829e47fa4733aee08e627b7162183cc17
chummer5isalive <chummer5isalive@gmail.com>
2020-01-17 22:54:57 +1100

    Adding SourceDetail stuff to Options, currently unused

--- Chummer/Classes/Options.cs
+++ Chummer/Classes/Options.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+using System.IO;
 using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
@@ -11,8 +11,8 @@ namespace Chummer.Classes
 		private string _strCategory = "";
 		private string _strDescriptionTag = "";
 		private string _strModifierTag = "";
-		private string _strSourcebook = "";
-		private string _strPage = "";
+        private string _strSource = "";
+        private string _strPage = "";
 		private object _objDefault;
 		private object _objValue;
 		#region Constructor Methods
@@ -31,8 +31,9 @@ namespace Chummer.Classes
 			objNode.TryGetField("category", out _strCategory);
 			objNode.TryGetField("descriptiontag", out _strDescriptionTag);
 			objNode.TryGetField("modifiertag", out _strModifierTag);
-			objNode.TryGetField("sourcebook", out _strModifierTag);
-			if (objNode["default"] != null)
+            objNode.TryGetStringFieldQuickly("source", ref _strSource);
+            objNode.TryGetStringFieldQuickly("page", ref _strPage);
+            if (objNode["default"] != null)
 			{
 				_objDefault = objNode["default"].InnerText;
 			}
@@ -53,10 +54,9 @@ namespace Chummer.Classes
 		/// </summary>
 		public string Name
 		{
-			get {return _strName;}
-			set { _strName = value; }
-			
-		}
+			get => _strName;
+            set => _strName = value;
+        }
 
 		/// <summary>
 		/// Proper name of the category in which the Option's control shows up in the Options menu. 
@@ -64,27 +64,27 @@ namespace Chummer.Classes
 		/// </summary>
 		public string Category
 		{
-			get { return _strCategory; }
-			set { _strCategory = value; }
-		}
+			get => _strCategory;
+            set => _strCategory = value;
+        }
 
 		/// <summary>
 		/// Default value of the Option.
 		/// </summary>
 		public object Default
 		{
-			get { return _objDefault; }
-			set { _objDefault = value; }
-		}
+			get => _objDefault;
+            set => _objDefault = value;
+        }
 
 		/// <summary>
 		/// Current value of the Option.
 		/// </summary>
 		public object Value
 		{
-			get {return _objValue;}
-			set { _objValue = value; } 
-		}
+			get => _objValue;
+            set => _objValue = value;
+        }
 
 		/// <summary>
 		/// Whether the option has been modified by the user. 
@@ -97,9 +97,9 @@ namespace Chummer.Classes
 		/// </summary>
 		public string DescriptionTag
 		{
-			get { return _strDescriptionTag; }
-			set { _strDescriptionTag = value; }
-		}
+			get => _strDescriptionTag;
+            set => _strDescriptionTag = value;
+        }
 
 		/// <summary>
 		/// Key string that will be used to fetch the secondary display text for the description from LanguageManager.
@@ -107,18 +107,46 @@ namespace Chummer.Classes
 		/// </summary>
 		public string ModifierTag
 		{
-			get { return _strModifierTag; }
-			set { _strModifierTag = value; }
-		}
+			get => _strModifierTag;
+            set => _strModifierTag = value;
+        }
 
-		/// <summary>
-		/// Sourceook that the rule comes from. Mostly used for Optional Rules.
-		/// </summary>
-		public string Sourcebook
-		{
-			get { return _strSourcebook; }
-			set { _strSourcebook = value; }
-		}
-		#endregion
-	}
+        /// <summary>
+        /// Sourcebook that the rule comes from. Mostly used for Optional Rules.
+        /// </summary>
+        public string Source
+        {
+            get => _strSource;
+            set => _strSource = value;
+        }
+
+        /// <summary>
+        /// Sourcebook Page Number.
+        /// </summary>
+        public string Page
+        {
+            get => _strPage;
+            set => _strPage = value;
+        }
+
+        /// <summary>
+        /// Sourcebook Page Number using a given language file.
+        /// Returns Page if not found or the string is empty.
+        /// </summary>
+        /// <param name="strLanguage">Language file keyword to use.</param>
+        /// <returns></returns>
+        public string DisplayPage(string strLanguage)
+        {
+            //if (strLanguage == GlobalOptions.DefaultLanguage)
+                return Page;
+            //string s = GetNode(strLanguage)?["altpage"]?.InnerText ?? Page;
+            //return !string.IsNullOrWhiteSpace(s) ? s : Page;
+        }
+
+        private SourceString _objCachedSourceDetail;
+        public SourceString SourceDetail => _objCachedSourceDetail ?? (_objCachedSourceDetail =
+                                                new SourceString(Source, DisplayPage(GlobalOptions.Language), GlobalOptions.Language));
+
+        #endregion
+    }
 }
