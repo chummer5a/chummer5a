8195b66b676465408024d6dbea4628ec5e2f677b
chummer5isalive <chummer5isalive@gmail.com>
2020-05-19 20:58:30 +1000

    Assemblyinfo patching

--- Chummer/Properties/AssemblyInfo.cs
+++ Chummer/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("5.211.0.0")]
-[assembly: AssemblyFileVersion("5.211.0.0")]
+[assembly: AssemblyVersion("5.212.0.0")]
+[assembly: AssemblyFileVersion("5.212.0.0")]
