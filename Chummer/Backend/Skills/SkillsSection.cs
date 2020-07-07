5b5fd8bd826c0e6a6c78c2a859a10935ee53804f
Delnar_Ersike <delnar.ersike@gmail.com>
2020-07-07 22:32:53 +0200

    Regression Fix for Lack of SkillGroup Section

--- Chummer/Backend/Skills/SkillsSection.cs
+++ Chummer/Backend/Skills/SkillsSection.cs
@@ -799,9 +799,9 @@ namespace Chummer.Backend.Skills
             foreach (KnowledgeSkill objKnowledgeSkill in KnowledgeSkills)
                 objKnowledgeSkill.UnbindSkill();
             KnowledgeSkills.Clear();
-            foreach (SkillGroup objGroup in SkillGroups)
+            foreach (SkillGroup objGroup in _lstSkillGroups)
                 objGroup.UnbindSkillGroup();
-            SkillGroups.Clear();
+            _lstSkillGroups.Clear();
             SkillPointsMaximum = 0;
             SkillGroupPointsMaximum = 0;
             foreach (KnowledgeSkill objKnowledgeSkill in KnowsoftSkills)
@@ -872,16 +872,38 @@ namespace Chummer.Backend.Skills
 
         public BindingList<KnowledgeSkill> KnowledgeSkills { get; } = new BindingList<KnowledgeSkill>();
 
-
         /// <summary>
         /// KnowsoftSkills.
         /// </summary>
         public IList<KnowledgeSkill> KnowsoftSkills { get; } = new List<KnowledgeSkill>();
 
+        private bool _blnSkillGroupsIsLoading;
+        private readonly BindingList<SkillGroup> _lstSkillGroups = new BindingList<SkillGroup>();
+
         /// <summary>
         /// Skill Groups.
         /// </summary>
-        public BindingList<SkillGroup> SkillGroups { get; } = new BindingList<SkillGroup>();
+        public BindingList<SkillGroup> SkillGroups
+        {
+            get
+            {
+                if (_lstSkillGroups.Count == 0 && !_blnSkillGroupsIsLoading)
+                {
+                    // Skill groups are loaded in with skills, so make sure to load them first
+                    if (_lstSkills.Count == 0)
+                    {
+                        _blnSkillGroupsIsLoading = true; // Needed to make sure _lstSkillGroups gets returned when a skill's loading function asks for it
+                        foreach (Skill objLoopSkill in GetSkillList(FilterOption.NonSpecial))
+                        {
+                            _lstSkills.Add(objLoopSkill);
+                            _dicSkills.Add(objLoopSkill.DictionaryKey, objLoopSkill);
+                        }
+                        _blnSkillGroupsIsLoading = false;
+                    }
+                }
+                return _lstSkillGroups;
+            }
+        }
 
         public bool HasKnowledgePoints => KnowledgeSkillPoints > 0;
 
