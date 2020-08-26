c8143c70052d51b6cefba35869d1f2620ca701c7
Delnar_Ersike <delnar.ersike@gmail.com>
2020-08-26 06:55:57 +0200

    Added in Initial LabelWithToolTip Stuff to Help with Font Theming

--- Chummer/UI/Skills/SkillControl2.Designer.cs
+++ Chummer/UI/Skills/SkillControl2.Designer.cs
@@ -252,7 +252,7 @@ namespace Chummer.UI.Skills
             // 
             this.lblModifiedRating.AutoSize = true;
             this.lblModifiedRating.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblModifiedRating.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModifiedRating.Bold = true;
             this.lblModifiedRating.Location = new System.Drawing.Point(172, 0);
             this.lblModifiedRating.MinimumSize = new System.Drawing.Size(50, 0);
             this.lblModifiedRating.Name = "lblModifiedRating";
