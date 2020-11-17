2490de92dd91ac76ab13a1aa4e254b8d51d3dde1
Delnar_Ersike <delnar.ersike@gmail.com>
2020-11-17 21:12:23 +0100

    frmSelectWeaponCategory DPI Scaling Fix

--- Chummer/Selection Forms/frmSelectWeaponCategory.Designer.cs
+++ Chummer/Selection Forms/frmSelectWeaponCategory.Designer.cs
@@ -132,6 +132,8 @@ namespace Chummer
             this.AcceptButton = this.cmdOK;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.CancelButton = this.cmdCancel;
             this.ClientSize = new System.Drawing.Size(344, 101);
             this.ControlBox = false;
