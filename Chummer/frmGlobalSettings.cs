59bcfaf5c525b8fbfc1cd7b405d11f97df64983f
Delnar_Ersike <delnar.ersike@gmail.com>
2021-11-03 22:55:58 +0100

    Made PDF Scanner Actually Release-Ready

--- Chummer/frmGlobalSettings.cs
+++ Chummer/frmGlobalSettings.cs
@@ -1515,18 +1515,18 @@ namespace Chummer
             // Prompt the user to select a save file to associate with this Contact.
             using (new CursorWait(this))
             {
-                using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+                Task<XPathNavigator> tskLoadBooks = XmlManager.LoadXPathAsync("books.xml", strLanguage: _strSelectedLanguage);
+                using (FolderBrowserDialog fbd = new FolderBrowserDialog {ShowNewFolderButton = false})
                 {
                     DialogResult result = fbd.ShowDialog();
 
                     if (result != DialogResult.OK || string.IsNullOrWhiteSpace(fbd.SelectedPath))
                         return;
-                    var sw = new Stopwatch();
+                    Stopwatch sw = new Stopwatch();
                     sw.Start();
-                    string[] files = Directory.GetFiles(fbd.SelectedPath);
-                    var books = await XmlManager.LoadXPathAsync("books.xml");
-                    string xpath = "/chummer/books/book/matches/match[language=\"" + GlobalSettings.Language + "\"]";
-                    XPathNodeIterator matches = books.Select(xpath);
+                    string[] files = Directory.GetFiles(fbd.SelectedPath, "*.pdf", SearchOption.AllDirectories);
+                    XPathNavigator books = await tskLoadBooks;
+                    XPathNodeIterator matches = books.Select("/chummer/books/book/matches/match[language=\"" + _strSelectedLanguage + "\"]");
                     using (frmLoading frmProgressBar = frmChummerMain.CreateAndShowProgressBar(fbd.SelectedPath, files.Length))
                     {
                         List<SourcebookInfo> list = null;
@@ -1539,11 +1539,11 @@ namespace Chummer
                                                     .AppendLine(
                                                         "-------------------------------------------------------------")
                                                     .AppendFormat(GlobalSettings.InvariantCultureInfo,
-                                                                  "Scan for PDFs in Folder {0} completed in {1}ms.{2}{3} Sourcebook(s) was/were found:",
+                                                                  "Scan for PDFs in Folder {0} completed in {1}ms.{2}{3} sourcebook(s) was/were found:",
                                                                   fbd.SelectedPath, sw.ElapsedMilliseconds,
                                                                   Environment.NewLine, list.Count).AppendLine()
                                                     .AppendLine();
-                        foreach(var sourcebook in list)
+                        foreach(SourcebookInfo sourcebook in list)
                         {
                             sbdFeedback.AppendFormat(GlobalSettings.InvariantCultureInfo,
                                                      "{0} with Offset {1} path: {2}", sourcebook.Code,
@@ -1554,69 +1554,13 @@ namespace Chummer
                                    .AppendLine("-------------------------------------------------------------");
                         Log.Info(sbdFeedback.ToString());
 
-                        var message = LanguageManager.GetString("Message_FoundPDFsInFolder") + list.Count;
-                        var title = LanguageManager.GetString("MessageTitle_FoundPDFsInFolder");
+                        var message = string.Format(_objSelectedCultureInfo, LanguageManager.GetString("Message_FoundPDFsInFolder", _strSelectedLanguage), list.Count, fbd.SelectedPath);
+                        var title = LanguageManager.GetString("MessageTitle_FoundPDFsInFolder", _strSelectedLanguage);
                     
-                        var dialogresult = Program.MainForm.ShowMessageBox(message, title, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                        if (dialogresult != DialogResult.Yes)
-                            return;
-                        //display the log
-                        string filename = GetLogFileName("f");
-                        FileInfo fileInfo = new FileInfo(filename);
-                        if (fileInfo.Exists)
-                        {
-                            Process.Start(fileInfo.FullName);
-                        }
+                        Program.MainForm.ShowMessageBox(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
                     }
                 }
             }
-        }
-
-        private string GetLogFileName(string targetName)
-        {
-            string fileName;
-
-            if (LogManager.Configuration != null && LogManager.Configuration.ConfiguredNamedTargets.Count != 0)
-            {
-                Target target = LogManager.Configuration.FindTargetByName(targetName);
-                if (target == null)
-                {
-                    throw new ArgumentException("Could not find target named: " + targetName);
-                }
-
-                FileTarget fileTarget;
-                // Unwrap the target if necessary.
-                if (!(target is WrapperTargetBase wrapperTarget))
-                {
-                    fileTarget = target as FileTarget;
-                }
-                else
-                {
-                    fileTarget = wrapperTarget.WrappedTarget as FileTarget;
-                }
-
-                if (fileTarget == null)
-                {
-                    throw new ArgumentException("Could not get a FileTarget from " + target.GetType());
-                }
-
-                var logEventInfo = new LogEventInfo { TimeStamp = DateTime.Now };
-                fileName = fileTarget.FileName.Render(logEventInfo);
-                var uri = new Uri(fileName);
-                fileName = uri.AbsolutePath;
-                
-            }
-            else
-            {
-                throw new InvalidOperationException("LogManager contains no Configuration or there are no named targets");
-            }
-
-            if (!File.Exists(fileName))
-            {
-                throw new FileNotFoundException("File " + fileName + " does not exist");
-            }
-
-            return fileName;
         }
 
         private ConcurrentBag<SourcebookInfo> ScanFilesForPDFTexts(string[] files, XPathNodeIterator matches, frmLoading frmProgressBar)
