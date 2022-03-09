72996beaa8ca5b6131159b27dd8d0842fe45d583
Delnar_Ersike <delnar.ersike@gmail.com>
2022-03-09 02:24:52 +0100

    Fixed a Weird Crash Caused by Odd Disposal Threading

--- Chummer/Forms/Utility Forms/LoadingBar.Designer.cs
+++ Chummer/Forms/Utility Forms/LoadingBar.Designer.cs
@@ -13,11 +13,15 @@ namespace Chummer
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
+            this.DoThreadSafe(() =>
             {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
+                if (disposing && (components != null))
+                {
+                    components.Dispose();
+                }
+
+                base.Dispose(disposing);
+            });
         }
 
         #region Windows Form Designer generated code
