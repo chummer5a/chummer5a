b1b4d4f082bd344e66c81e4a8aee9b04bb96d8e4
Delnar_Ersike <delnar.ersike@gmail.com>
2022-03-28 23:21:19 +0200

    Removed Mdi Rendering "Tricks" Due to Compatibility Problems

--- Chummer/Forms/ChummerMainForm.cs
+++ Chummer/Forms/ChummerMainForm.cs
@@ -77,27 +77,6 @@ namespace Chummer
             this.TranslateWinForm();
             _strCurrentVersion = Utils.CurrentChummerVersion.ToString(3);
 
-            // Retrieve the MDI client control on this parent window and apply some rendering tweaks to it.
-            foreach (Control control in Controls)
-            {
-                MdiClient mdiClient = control as MdiClient;
-                if (mdiClient == null)
-                    continue;
-                // Force double buffering on it by calling the protected SetStyle method via reflection.
-                MethodInfo setStyleMethod = typeof(MdiClient).GetMethod("SetStyle", BindingFlags.NonPublic | BindingFlags.Instance);
-                if (setStyleMethod != null)
-                {
-                    object[] styleParams =
-                    {
-                        ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer
-                                                           | ControlStyles.ResizeRedraw,
-                        true
-                    };
-                    setStyleMethod.Invoke(mdiClient, styleParams);
-                }
-                break;
-            }
-
             //lets write that in separate lines to see where the exception is thrown
             if (!GlobalSettings.HideMasterIndex || isUnitTest)
             {
