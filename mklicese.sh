5a5eed779d78e271f6a8097dab676802407e7979
Delnar_Ersike <delnar.ersike@gmail.com>
2022-05-04 23:23:16 +0200

    License Script Update

--- mklicese.sh
+++ mklicese.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-find . -type f -name '*.cs' ! -name '*.Designer.cs' -print0 | while IfS= read -r -d '' file; do
+find . -type f -name '*.cs' ! -name '*.Designer.cs' ! -ipath 'packages/*' ! -ipath '*/packages/*' ! -ipath 'bin/*' ! -ipath '*/bin/*' -print0 | while IfS= read -r -d '' file; do
   if ! grep -q 'General Public License' "$file"
   then
     echo Processing cs \" "$file" \" 1>&2  
@@ -8,7 +8,7 @@ find . -type f -name '*.cs' ! -name '*.Designer.cs' -print0 | while IfS= read -r
   fi
 done
 
-find . -type f -name '*.xml' -print0 | while Ifs= read -r -d '' file; do
+find . -type f -name '*.xml' ! -ipath 'packages/*' ! -ipath '*/packages/*' ! -ipath 'bin/*' ! -ipath '*/bin/*' -print0 | while Ifs= read -r -d '' file; do
   if ! grep -q 'General Public License' "$file"
   then
     echo Processing Xml \" "$file" \" 1>&2
