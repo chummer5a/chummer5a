7320700c5be83fa5f97ff5ebc5c4779bd9450550
Delnar_Ersike <delnar.ersike@gmail.com>
2022-09-24 16:39:48 +0200

    Switched Image <-> String Conversion Methods to Use ArrayPool<byte>
    Because images tend to be fairly large data-wise, using ArrayPool results in faster conversions because the allocations for using new arrays would take so long.

---
+++ Chummer/Backend/Static/Extensions/StreamExtensions.cs
@@ -0,0 +1,67 @@
+/*  This file is part of Chummer5a.
+ *
+ *  Chummer5a is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Chummer5a is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Chummer5a.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  You can obtain the full source code for Chummer5a at
+ *  https://github.com/chummer5a/chummer5a
+ */
+
+using System;
+using System.Buffers;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Array = System.Array;
+
+namespace Chummer
+{
+    public static class StreamExtensions
+    {
+        public static byte[] ToPooledArray(this MemoryStream objStream)
+        {
+            objStream.Seek(0, SeekOrigin.Begin);
+            int intLength = Convert.ToInt32(objStream.Length);
+            byte[] achrReturn = ArrayPool<byte>.Shared.Rent(intLength);
+            try
+            {
+                Array.Clear(achrReturn, 0, achrReturn.Length);
+                _ = objStream.Read(achrReturn, 0, intLength);
+            }
+            catch
+            {
+                ArrayPool<byte>.Shared.Return(achrReturn);
+                throw;
+            }
+            return achrReturn;
+        }
+
+        public static async Task<byte[]> ToPooledArrayAsync(this MemoryStream objStream, CancellationToken token = default)
+        {
+            objStream.Seek(0, SeekOrigin.Begin);
+            int intLength = Convert.ToInt32(objStream.Length);
+            byte[] achrReturn = ArrayPool<byte>.Shared.Rent(intLength);
+            try
+            {
+                Array.Clear(achrReturn, 0, achrReturn.Length);
+                _ = await objStream.ReadAsync(achrReturn, 0, intLength, token);
+            }
+            catch
+            {
+                ArrayPool<byte>.Shared.Return(achrReturn);
+                throw;
+            }
+            return achrReturn;
+        }
+    }
+}
--- Chummer/Backend/Static/Extensions/StringExtensions.cs
+++ Chummer/Backend/Static/Extensions/StringExtensions.cs
@@ -1638,5 +1638,264 @@ namespace Chummer
 
         private static readonly DebuggableSemaphoreSlim s_RtbRtfManipulatorLock = new DebuggableSemaphoreSlim();
         private static readonly RichTextBox s_RtbRtfManipulator = new RichTextBox();
+
+        /// <summary>
+        /// Converts the specified string, which encodes binary data as base-64 digits, to an equivalent 8-bit unsigned integer array.
+        /// Nearly identical to Convert.FromBase64String(), but the byte array that's returned is from a shared ArrayPool instead of newly allocated.
+        /// </summary>
+        /// <param name="s">The string to convert.</param>
+        /// <returns>A rented array (from ArrayPool.Shared) of 8-bit unsigned integers that is equivalent to s.</returns>
+        /// <exception cref="ArgumentNullException">s is null.</exception>
+        /// <exception cref="FormatException">The length of s, ignoring white-space characters, is not zero or a multiple of 4. -or-The format of s is invalid. s contains a non-base-64 character, more than two padding characters, or a non-white space-character among the padding characters.</exception>
+        public static byte[] ToBase64PooledByteArray(this string s)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException(nameof(s));
+            }
+
+            unchecked
+            {
+                unsafe
+                {
+                    fixed (char* inputPtr = s)
+                    {
+                        int inputLength = s.Length;
+                        while (inputLength > 0)
+                        {
+                            int num = inputPtr[inputLength - 1];
+                            if (num != 32 && num != 10 && num != 13 && num != 9)
+                            {
+                                break;
+                            }
+
+                            inputLength--;
+                        }
+
+                        int num2 = FromBase64_ComputeResultLength(inputPtr, inputLength);
+                        byte[] array = ArrayPool<byte>.Shared.Rent(num2);
+                        Array.Clear(array, 0, array.Length);
+                        try
+                        {
+                            fixed (byte* startDestPtr = array)
+                            {
+                                _ = FromBase64_Decode(inputPtr, inputLength, startDestPtr, num2);
+                            }
+
+                            return array;
+                        }
+                        catch
+                        {
+                            ArrayPool<byte>.Shared.Return(array, true);
+                            throw;
+                        }
+                    }
+
+                    int FromBase64_ComputeResultLength(char* inputPtr, int inputLength)
+                    {
+                        char* ptr = inputPtr + inputLength;
+                        int num = inputLength;
+                        int num2 = 0;
+                        while (inputPtr < ptr)
+                        {
+                            uint num3 = *inputPtr;
+                            inputPtr++;
+                            switch (num3)
+                            {
+                                case 0u:
+                                case 1u:
+                                case 2u:
+                                case 3u:
+                                case 4u:
+                                case 5u:
+                                case 6u:
+                                case 7u:
+                                case 8u:
+                                case 9u:
+                                case 10u:
+                                case 11u:
+                                case 12u:
+                                case 13u:
+                                case 14u:
+                                case 15u:
+                                case 16u:
+                                case 17u:
+                                case 18u:
+                                case 19u:
+                                case 20u:
+                                case 21u:
+                                case 22u:
+                                case 23u:
+                                case 24u:
+                                case 25u:
+                                case 26u:
+                                case 27u:
+                                case 28u:
+                                case 29u:
+                                case 30u:
+                                case 31u:
+                                case 32u:
+                                    num--;
+                                    break;
+                                case 61u:
+                                    num--;
+                                    num2++;
+                                    break;
+                            }
+                        }
+
+                        switch (num2)
+                        {
+                            case 1:
+                                num2 = 2;
+                                break;
+                            case 2:
+                                num2 = 1;
+                                break;
+                            case 0:
+                                break;
+                            default:
+                                throw new FormatException(
+                                    "The input is not a valid Base-64 string as it contains a non-base 64 character, more than two padding characters, or an illegal character among the padding characters.");
+                        }
+
+                        return num / 4 * 3 + num2;
+                    }
+
+                    int FromBase64_Decode(char* startInputPtr, int inputLength, byte* startDestPtr, int destLength)
+                    {
+                        char* ptr = startInputPtr;
+                        byte* ptr2 = startDestPtr;
+                        char* ptr3 = ptr + inputLength;
+                        byte* ptr4 = ptr2 + destLength;
+                        uint num = 255u;
+                        while (ptr < ptr3)
+                        {
+                            uint num2 = *ptr;
+                            ptr++;
+                            if (num2 - 65 <= 25)
+                            {
+                                num2 -= 65;
+                            }
+                            else if (num2 - 97 <= 25)
+                            {
+                                num2 -= 71;
+                            }
+                            else if (num2 - 48 <= 9)
+                            {
+                                num2 -= 4294967292u;
+                            }
+                            else
+                            {
+                                if (num2 <= 32)
+                                {
+                                    if (num2 - 9 <= 1 || num2 == 13 || num2 == 32)
+                                    {
+                                        continue;
+                                    }
+
+                                    goto IL_0097;
+                                }
+
+                                if (num2 != 43)
+                                {
+                                    if (num2 != 47)
+                                    {
+                                        if (num2 != 61)
+                                        {
+                                            goto IL_0097;
+                                        }
+
+                                        if (ptr == ptr3)
+                                        {
+                                            num <<= 6;
+                                            if ((num & 0x80000000u) == 0)
+                                            {
+                                                throw new FormatException(
+                                                    "Invalid length for a Base-64 char array or string.");
+                                            }
+
+                                            if ((int) (ptr4 - ptr2) < 2)
+                                            {
+                                                return -1;
+                                            }
+
+                                            *(ptr2++) = (byte) (num >> 16);
+                                            *(ptr2++) = (byte) (num >> 8);
+                                            num = 255u;
+                                            break;
+                                        }
+
+                                        for (; ptr < ptr3 - 1; ptr++)
+                                        {
+                                            int num3 = *ptr;
+                                            if (num3 != 32 && num3 != 10 && num3 != 13 && num3 != 9)
+                                            {
+                                                break;
+                                            }
+                                        }
+
+                                        if (ptr == ptr3 - 1 && *ptr == '=')
+                                        {
+                                            num <<= 12;
+                                            if ((num & 0x80000000u) == 0)
+                                            {
+                                                throw new FormatException(
+                                                    "Invalid length for a Base-64 char array or string.");
+                                            }
+
+                                            if ((int) (ptr4 - ptr2) < 1)
+                                            {
+                                                return -1;
+                                            }
+
+                                            *(ptr2++) = (byte) (num >> 16);
+                                            num = 255u;
+                                            break;
+                                        }
+
+                                        throw new FormatException(
+                                            "The input is not a valid Base-64 string as it contains a non-base 64 character, more than two padding characters, or an illegal character among the padding characters.");
+                                    }
+
+                                    num2 = 63u;
+                                }
+                                else
+                                {
+                                    num2 = 62u;
+                                }
+                            }
+
+                            num = (num << 6) | num2;
+                            if ((num & 0x80000000u) != 0)
+                            {
+                                if ((int) (ptr4 - ptr2) < 3)
+                                {
+                                    return -1;
+                                }
+
+                                *ptr2 = (byte) (num >> 16);
+                                ptr2[1] = (byte) (num >> 8);
+                                ptr2[2] = (byte) num;
+                                ptr2 += 3;
+                                num = 255u;
+                            }
+
+                            continue;
+                            IL_0097:
+                            throw new FormatException(
+                                "The input is not a valid Base-64 string as it contains a non-base 64 character, more than two padding characters, or an illegal character among the padding characters.");
+                        }
+
+                        if (num != 255)
+                        {
+                            throw new FormatException("Invalid length for a Base-64 char array or string.");
+                        }
+
+                        return (int) (ptr2 - startDestPtr);
+                    }
+                }
+            }
+        }
     }
 }
