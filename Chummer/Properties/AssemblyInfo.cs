ab00778f30a6e4f1ad3fa7e340c0de97113bc814
Joschi <joschi.grey@posteo.de>
2023-08-29 23:13:13 +0200

    Updated AssemblyInfo and app.manifest

---
+++ Chummer/Properties/AssemblyInfo.cs
@@ -0,0 +1,52 @@
+/*  This file is part of Chummer5a.
+ *
+ *  Chummer5a is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Chummer5a is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Chummer5a.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  You can obtain the full source code for Chummer5a at
+ *  https://github.com/chummer5a/chummer5a
+ */
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Chummer")]
+[assembly: AssemblyDescription("Character generator for Shadowrun 5th Edition")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Chummer5a")]
+[assembly: AssemblyProduct("Chummer5a")]
+[assembly: AssemblyCopyright("Copyright © Keith Rudolph 2013, Licensed GNU GPLv3")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("28a46ea3-4e45-4842-b8ae-313afde61173")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+[assembly: AssemblyVersion("5.225.0.0")]
+[assembly: AssemblyFileVersion("5.225.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
