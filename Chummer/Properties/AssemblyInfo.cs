485a37fd75a7fc0b6da7b2f43c013d4d05b216e8
Joschi <joschi.grey@posteo.de>
2023-08-31 00:54:48 +0200

    Marked the Chummer Assembly as windows only

--- Chummer/Properties/AssemblyInfo.cs
+++ Chummer/Properties/AssemblyInfo.cs
@@ -41,6 +41,9 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("28a46ea3-4e45-4842-b8ae-313afde61173")]
 
+// Currently this is only for windows
+[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("windows")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
