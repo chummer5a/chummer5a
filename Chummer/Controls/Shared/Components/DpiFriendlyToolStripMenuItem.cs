59fe9bad94398bf28fe342db03437bf5b9adb581
Delnar_Ersike <delnar.ersike@gmail.com>
2025-07-30 19:20:45 +0200

    Fixed Some Weird High DPI Scaling Issues

--- Chummer/Controls/Shared/Components/DpiFriendlyToolStripMenuItem.cs
+++ Chummer/Controls/Shared/Components/DpiFriendlyToolStripMenuItem.cs
@@ -83,16 +83,22 @@ namespace Chummer
             }
 
             // Toolstrip items contain both images and text, so we take the smallest of the two dimensions for the image and then assume that the image should be square-shaped
-            int intWidth = Math.Min(Width, Height);
-            int intHeight = Math.Min(Width, Height);
-            intWidth -= Padding.Left + Padding.Right;
-            intHeight -= Padding.Top + Padding.Bottom;
+            int intWidth;
+            if (Parent != null)
+            {
+                Size ePreferredImageSize = Parent.ImageScalingSize;
+                // No Padding incorporated into preferred image size because it's also ignored on the toolstrip side
+                intWidth = Math.Min(ePreferredImageSize.Width, ePreferredImageSize.Height);
+            }
+            else
+                intWidth = Math.Min(Width - (Padding.Left + Padding.Right), Height - (Padding.Top + Padding.Bottom));
+            int intHeight = intWidth;
             Image objBestImage = null;
             int intBestImageMetric = int.MaxValue;
             foreach (Image objLoopImage in lstImages)
             {
                 int intLoopMetric = (intHeight - objLoopImage.Height).Pow(2) + (intWidth - objLoopImage.Width).Pow(2);
-                // Small biasing so that in case of a tie, the image that gets picked is the one that would be scaled down, not scaled up
+                // Small biasing so that in case of a tie, the image that gets picked is the one that would be scaled down, not up
                 if (objLoopImage.Height >= intHeight)
                     --intLoopMetric;
                 if (objLoopImage.Width >= intWidth)
@@ -261,15 +267,31 @@ namespace Chummer
                 Image = objNewImage;
                 return;
             }
+
             // Toolstrip items contain both images and text, so we take the smallest of the two dimensions for the image and then assume that the image should be square-shaped
-            int intWidth = Math.Min(Width, Height);
-            int intHeight = Math.Min(Width, Height);
-            intWidth -= Padding.Left + Padding.Right;
-            intHeight -= Padding.Top + Padding.Bottom;
+            int intWidth;
+            if (Parent != null)
+            {
+                Size ePreferredImageSize = Parent.ImageScalingSize;
+                // No Padding incorporated into preferred image size because it's also ignored on the toolstrip side
+                intWidth = Math.Min(ePreferredImageSize.Width, ePreferredImageSize.Height);
+            }
+            else
+                intWidth = Math.Min(Width - (Padding.Left + Padding.Right), Height - (Padding.Top + Padding.Bottom));
+            int intHeight = intWidth;
             int intCurrentMetric = (intHeight - Image.Height).Pow(2) +
                                    (intWidth - Image.Width).Pow(2);
             int intNewMetric = (intHeight - objNewImage.Height).Pow(2) +
                                (intWidth - objNewImage.Width).Pow(2);
+            // Small biasing so that in case of a tie, the image that gets picked is the one that would be scaled down, not up
+            if (Image.Height >= intHeight)
+                --intCurrentMetric;
+            if (Image.Width >= intWidth)
+                --intCurrentMetric;
+            if (objNewImage.Height >= intHeight)
+                --intNewMetric;
+            if (objNewImage.Width >= intWidth)
+                --intNewMetric;
             if (intNewMetric < intCurrentMetric)
                 Image = objNewImage;
         }
@@ -283,6 +305,33 @@ namespace Chummer
         protected override void OnTextChanged(EventArgs e)
         {
             base.OnTextChanged(e);
+            RefreshImage();
+        }
+
+        protected override void OnBoundsChanged()
+        {
+            base.OnBoundsChanged();
+            RefreshImage();
+        }
+
+        protected override void OnParentChanged(ToolStrip oldParent, ToolStrip newParent)
+        {
+            base.OnParentChanged(oldParent, newParent);
+            if (oldParent != null)
+            {
+                oldParent.DpiChangedAfterParent -= RefreshImage;
+                oldParent.SizeChanged -= RefreshImage;
+            }
+            if (newParent != null)
+            {
+                newParent.DpiChangedAfterParent += RefreshImage;
+                newParent.SizeChanged += RefreshImage;
+            }
+            RefreshImage();
+        }
+
+        private void RefreshImage(object sender, EventArgs e)
+        {
             RefreshImage();
         }
     }
