a85bdb0bf98d2fb550a452093af26b2742bca647
Delnar_Ersike <delnar.ersike@gmail.com>
2025-09-06 17:19:46 +0200

    Expanded TemporaryArray Some More

--- Chummer/Backend/Helpers/TemporaryArray.cs
+++ Chummer/Backend/Helpers/TemporaryArray.cs
@@ -21,6 +21,8 @@ using System;
 using System.Buffers;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Chummer
 {
@@ -28,12 +30,174 @@ namespace Chummer
     /// Helping wrapper around an array taken from the shared array pool that will also respect the desired input size of the array when being read or enumerated.
     /// Should only be used as a way to prevent excessive heap allocations for small arrays that are only meant to be used a handful of times (but cannot be stackalloc'ed because they are for reference types).
     /// </summary>
-    public readonly struct TemporaryArray<T> : IReadOnlyList<T>, IDisposable
+    public readonly struct TemporaryArray<T> : IReadOnlyList<T>, IDisposable, IEquatable<TemporaryArray<T>>
     {
         private readonly int _intSize;
         private readonly T[] _aobjInternal;
 
         // Constructors do not use params keyword because that will result in (undesired) array allocation on the heap
+
+        public TemporaryArray(T[] items)
+        {
+            _intSize = items.Length;
+            if (_intSize == 0)
+                _aobjInternal = Array.Empty<T>();
+            else
+            {
+                _aobjInternal = ArrayPool<T>.Shared.Rent(_intSize);
+                Array.Copy(items, _aobjInternal, _intSize);
+            }
+        }
+
+        public TemporaryArray(ICollection<T> items)
+        {
+            _intSize = items.Count;
+            if (_intSize == 0)
+                _aobjInternal = Array.Empty<T>();
+            else
+            {
+                _aobjInternal = ArrayPool<T>.Shared.Rent(_intSize);
+                int i = 0;
+                foreach (T item in items)
+                    _aobjInternal[i++] = item;
+            }
+        }
+
+        public TemporaryArray(IReadOnlyCollection<T> items)
+        {
+            _intSize = items.Count;
+            if (_intSize == 0)
+                _aobjInternal = Array.Empty<T>();
+            else
+            {
+                _aobjInternal = ArrayPool<T>.Shared.Rent(_intSize);
+                int i = 0;
+                foreach (T item in items)
+                    _aobjInternal[i++] = item;
+            }
+        }
+
+        public TemporaryArray(IList<T> items)
+        {
+            _intSize = items.Count;
+            if (_intSize == 0)
+                _aobjInternal = Array.Empty<T>();
+            else
+            {
+                _aobjInternal = ArrayPool<T>.Shared.Rent(_intSize);
+                for (int i = 0; i < _intSize; ++i)
+                    _aobjInternal[i] = items[i];
+            }
+        }
+
+        public TemporaryArray(IReadOnlyList<T> items)
+        {
+            _intSize = items.Count;
+            if (_intSize == 0)
+                _aobjInternal = Array.Empty<T>();
+            else
+            {
+                _aobjInternal = ArrayPool<T>.Shared.Rent(_intSize);
+                for (int i = 0; i < _intSize; ++i)
+                    _aobjInternal[i] = items[i];
+            }
+        }
+
+        public TemporaryArray(IEnumerable<T> items)
+        {
+            using (IEnumerator<T> enumerator = items.GetEnumerator())
+            {
+                if (enumerator.MoveNext())
+                {
+                    T item1 = enumerator.Current;
+                    if (enumerator.MoveNext())
+                    {
+                        T item2 = enumerator.Current;
+                        if (enumerator.MoveNext())
+                        {
+                            T item3 = enumerator.Current;
+                            if (enumerator.MoveNext())
+                            {
+                                T item4 = enumerator.Current;
+                                if (enumerator.MoveNext())
+                                {
+                                    T item5 = enumerator.Current;
+                                    if (enumerator.MoveNext())
+                                    {
+                                        T item6 = enumerator.Current;
+                                        if (enumerator.MoveNext())
+                                        {
+                                            T item7 = enumerator.Current;
+                                            if (enumerator.MoveNext())
+                                            {
+                                                T item8 = enumerator.Current;
+                                                if (enumerator.MoveNext())
+                                                {
+                                                    throw new ArgumentOutOfRangeException(nameof(items));
+                                                }
+                                                else
+                                                {
+                                                    _intSize = 8;
+                                                    _aobjInternal = ArrayPool<T>.Shared.Rent(8);
+                                                }
+                                                _aobjInternal[7] = item8;
+                                            }
+                                            else
+                                            {
+                                                _intSize = 7;
+                                                _aobjInternal = ArrayPool<T>.Shared.Rent(7);
+                                            }
+                                            _aobjInternal[6] = item7;
+                                        }
+                                        else
+                                        {
+                                            _intSize = 6;
+                                            _aobjInternal = ArrayPool<T>.Shared.Rent(6);
+                                        }
+                                        _aobjInternal[5] = item6;
+                                    }
+                                    else
+                                    {
+                                        _intSize = 5;
+                                        _aobjInternal = ArrayPool<T>.Shared.Rent(5);
+                                    }
+                                    _aobjInternal[4] = item5;
+                                }
+                                else
+                                {
+                                    _intSize = 4;
+                                    _aobjInternal = ArrayPool<T>.Shared.Rent(4);
+                                }
+                                _aobjInternal[3] = item4;
+                            }
+                            else
+                            {
+                                _intSize = 3;
+                                _aobjInternal = ArrayPool<T>.Shared.Rent(3);
+                            }
+                            _aobjInternal[2] = item3;
+                        }
+                        else
+                        {
+                            _intSize = 2;
+                            _aobjInternal = ArrayPool<T>.Shared.Rent(2);
+                        }
+                        _aobjInternal[1] = item2;
+                    }
+                    else
+                    {
+                        _intSize = 1;
+                        _aobjInternal = ArrayPool<T>.Shared.Rent(1);
+                    }
+                    _aobjInternal[0] = item1;
+                }
+                else
+                {
+                    _intSize = 0;
+                    _aobjInternal = Array.Empty<T>();
+                }
+            }
+        }
 
         public TemporaryArray(T item1)
         {
@@ -69,6 +233,136 @@ namespace Chummer
             _aobjInternal[3] = item4;
         }
 
+        public TemporaryArray(T item1, T item2, T item3, T item4, T item5)
+        {
+            _intSize = 5;
+            _aobjInternal = ArrayPool<T>.Shared.Rent(5);
+            _aobjInternal[0] = item1;
+            _aobjInternal[1] = item2;
+            _aobjInternal[2] = item3;
+            _aobjInternal[3] = item4;
+            _aobjInternal[4] = item5;
+        }
+
+        public TemporaryArray(T item1, T item2, T item3, T item4, T item5, T item6)
+        {
+            _intSize = 6;
+            _aobjInternal = ArrayPool<T>.Shared.Rent(6);
+            _aobjInternal[0] = item1;
+            _aobjInternal[1] = item2;
+            _aobjInternal[2] = item3;
+            _aobjInternal[3] = item4;
+            _aobjInternal[4] = item5;
+            _aobjInternal[5] = item6;
+        }
+
+        public TemporaryArray(T item1, T item2, T item3, T item4, T item5, T item6, T item7)
+        {
+            _intSize = 7;
+            _aobjInternal = ArrayPool<T>.Shared.Rent(7);
+            _aobjInternal[0] = item1;
+            _aobjInternal[1] = item2;
+            _aobjInternal[2] = item3;
+            _aobjInternal[3] = item4;
+            _aobjInternal[4] = item5;
+            _aobjInternal[5] = item6;
+            _aobjInternal[6] = item7;
+        }
+
+        public TemporaryArray(T item1, T item2, T item3, T item4, T item5, T item6, T item7, T item8)
+        {
+            _intSize = 8;
+            _aobjInternal = ArrayPool<T>.Shared.Rent(8);
+            _aobjInternal[0] = item1;
+            _aobjInternal[1] = item2;
+            _aobjInternal[2] = item3;
+            _aobjInternal[3] = item4;
+            _aobjInternal[4] = item5;
+            _aobjInternal[5] = item6;
+            _aobjInternal[6] = item7;
+            _aobjInternal[7] = item8;
+        }
+
+        public static async Task<TemporaryArray<T>> NewAsync(IAsyncEnumerable<T> items, CancellationToken token = default)
+        {
+            token.ThrowIfCancellationRequested();
+            using (IEnumerator<T> enumerator = await items.GetEnumeratorAsync(token).ConfigureAwait(false))
+            {
+                if (enumerator.MoveNext())
+                {
+                    T item1 = enumerator.Current;
+                    if (enumerator.MoveNext())
+                    {
+                        T item2 = enumerator.Current;
+                        if (enumerator.MoveNext())
+                        {
+                            T item3 = enumerator.Current;
+                            if (enumerator.MoveNext())
+                            {
+                                T item4 = enumerator.Current;
+                                if (enumerator.MoveNext())
+                                {
+                                    T item5 = enumerator.Current;
+                                    if (enumerator.MoveNext())
+                                    {
+                                        T item6 = enumerator.Current;
+                                        if (enumerator.MoveNext())
+                                        {
+                                            T item7 = enumerator.Current;
+                                            if (enumerator.MoveNext())
+                                            {
+                                                T item8 = enumerator.Current;
+                                                if (enumerator.MoveNext())
+                                                {
+                                                    throw new ArgumentOutOfRangeException(nameof(items));
+                                                }
+                                                else
+                                                {
+                                                    return new TemporaryArray<T>(item1, item2, item3, item4, item5, item6, item7, item8);
+                                                }
+                                            }
+                                            else
+                                            {
+                                                return new TemporaryArray<T>(item1, item2, item3, item4, item5, item6, item7);
+                                            }
+                                        }
+                                        else
+                                        {
+                                            return new TemporaryArray<T>(item1, item2, item3, item4, item5, item6);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        return new TemporaryArray<T>(item1, item2, item3, item4, item5);
+                                    }
+                                }
+                                else
+                                {
+                                    return new TemporaryArray<T>(item1, item2, item3, item4);
+                                }
+                            }
+                            else
+                            {
+                                return new TemporaryArray<T>(item1, item2, item3);
+                            }
+                        }
+                        else
+                        {
+                            return new TemporaryArray<T>(item1, item2);
+                        }
+                    }
+                    else
+                    {
+                        return new TemporaryArray<T>(item1);
+                    }
+                }
+                else
+                {
+                    return new TemporaryArray<T>(Array.Empty<T>());
+                }
+            }
+        }
+
         public T this[int index]
         {
             get
@@ -92,7 +386,8 @@ namespace Chummer
 
         public void Dispose()
         {
-            ArrayPool<T>.Shared.Return(_aobjInternal);
+            if (Count > 0)
+                ArrayPool<T>.Shared.Return(_aobjInternal);
         }
 
         public IEnumerator<T> GetEnumerator()
@@ -103,6 +398,31 @@ namespace Chummer
         IEnumerator IEnumerable.GetEnumerator()
         {
             return new SZArrayEnumerator(_aobjInternal, Count);
+        }
+
+        public bool Equals(TemporaryArray<T> other)
+        {
+            return RawArray.Equals(other.RawArray);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is TemporaryArray<T> && Equals((TemporaryArray<T>)obj);
+        }
+
+        public static bool operator ==(TemporaryArray<T> left, TemporaryArray<T> right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(TemporaryArray<T> left, TemporaryArray<T> right)
+        {
+            return !(left == right);
+        }
+
+        public override int GetHashCode()
+        {
+            return RawArray.GetHashCode();
         }
 
         [Serializable]
